dd0d8cfe62a14110cab4167c8349f6eabd0b8e52
Vitaly Dragun <vdrag00n90@gmail.com>
2017-12-19 17:40:40 +0200

    Pro Angular ExampleApp - Unit Testing chapter - initial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,24 +1,9 @@
 import {RouterModule, Routes} from '@angular/router';
-import {FormComponent} from './core/form.component';
-import {TableComponent} from './core/table.component';
-import {NotFoundComponent} from './core/notFound.component';
-import {TermsGuard} from './terms.guard';
-import {UnsavedGuard} from './core/unsaved.guard';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/table', pathMatch: 'full'},
-  {
-    path: 'form/:mode/:id', component: FormComponent,
-    canDeactivate: [UnsavedGuard]
-  },
-  {
-    path: 'form/:mode', component: FormComponent,
-    canActivate: [TermsGuard]
-  },
-  {path: 'table/:category', component: TableComponent},
-  {path: 'table', component: TableComponent},
-  {path: '**', component: NotFoundComponent}
+  {path: '', redirectTo: '/ondemand', pathMatch: 'full'},
+  {path: 'ondemand', loadChildren: 'app/ondemand/ondemand.module#OndemandModule'}
 ];
 
 export const routing = RouterModule.forRoot(routes);
